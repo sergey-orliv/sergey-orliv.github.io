77585f1b493d62197fb04b89fd8af32e8b772619
sergey-orlov <serega_arlov@mail.ru>
2019-01-04 17:48:32 +0300

    new

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -1,9 +1,4 @@
 $(document).ready(function($) {
-   $(window).on('load', function() {
-    setTimeout(function() {
-      $('.preloader').fadeOut('slow', function() {});
-   }, 1000);
-   });
    var $page = $('html, body');
    $('a[href*="#"]').click(function() {
     $page.animate({
